ee52c2dafe299b661555cf4e13ff92f6792c97ba
DazWilkin <daz.wilkin@gmail.com>
2019-12-14 10:38:46 -0800

    Builder

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker build \
+--rm \
+--file=Dockerfile \
+--tag=dazwilkin/linode-exporter:$(git rev-parse HEAD) .
+
+docker push dazwilkin/linode-exporter:$(git rev-parse HEAD)
+
+sed --in-place "s|dazwilkin/linode-exporter:[0-9a-f]\{40\}|dazwilkin/linode-exporter:$(git rev-parse HEAD)|g" ./docker-compose.yaml
