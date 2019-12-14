df1ef9122dd1f5cd49f34bf7626a4c657381e316
DazWilkin <daz.wilkin@gmail.com>
2019-12-13 17:03:58 -0800

    Added CPU Ave|Max calculations

--- collector/instance.go
+++ collector/instance.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"sync"
 
 	"github.com/linode/linodego"
 	"github.com/prometheus/client_golang/prometheus"
@@ -13,8 +14,9 @@ import (
 type InstanceCollector struct {
 	client linodego.Client
 
-	Count *prometheus.Desc
-	CPU   *prometheus.Desc
+	Count  *prometheus.Desc
+	CPUAve *prometheus.Desc
+	CPUMax *prometheus.Desc
 }
 
 // NewInstanceCollector creates an InstanceCollector
@@ -29,9 +31,15 @@ func NewInstanceCollector(client linodego.Client) *InstanceCollector {
 			labelKeys,
 			nil,
 		),
-		CPU: prometheus.NewDesc(
-			"linode_instance_cpu_utilization",
-			"The most recent CPU utilization value",
+		CPUAve: prometheus.NewDesc(
+			"linode_instance_cpu_average_utilization",
+			"The most recent CPU average utilization value",
+			labelKeys,
+			nil,
+		),
+		CPUMax: prometheus.NewDesc(
+			"linode_instance_cpu_max_utilization",
+			"The most recent CPU max utilization value",
 			labelKeys,
 			nil,
 		),
@@ -58,34 +66,56 @@ func (c *InstanceCollector) Collect(ch chan<- prometheus.Metric) {
 		[]string{"", "", ""}...,
 	)
 
+	var wg sync.WaitGroup
 	for _, instance := range instances {
 		log.Printf("[InstanceCollector:Collect] Linode ID (%d)", instance.ID)
-		labelValues := []string{
-			fmt.Sprintf("%d", instance.ID),
-			instance.Label,
-			instance.Region,
-		}
-		stats, err := c.client.GetInstanceStats(ctx, instance.ID)
-		if err != nil {
-			log.Fatal(err)
-		}
 
-		// https://developers.linode.com/api/v4/linode-instances-linode-id-stats
-		// for _, cpu := range stats.Data.CPU {
-		ch <- prometheus.MustNewConstMetric(
-			c.CPU,
-			prometheus.GaugeValue,
-			stats.Data.CPU[0][0],
-			labelValues...,
-		)
-		// }
+		wg.Add(1)
+		go func(instance linodego.Instance) {
+			defer wg.Done()
+			labelValues := []string{
+				fmt.Sprintf("%d", instance.ID),
+				instance.Label,
+				instance.Region,
+			}
 
+			// https://developers.linode.com/api/v4/linode-instances-linode-id-stats
+			// Appears (!) to be 64 values (always) [0] == epoch in ms? [1] == value
+			stats, err := c.client.GetInstanceStats(ctx, instance.ID)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			var total, max float64
+			for _, cpu := range stats.Data.CPU {
+				if cpu[1] != 0.0 {
+					total += cpu[1]
+					if cpu[1] > max {
+						max = cpu[1]
+					}
+				}
+			}
+
+			ch <- prometheus.MustNewConstMetric(
+				c.CPUAve,
+				prometheus.GaugeValue,
+				total/float64(len(stats.Data.CPU)),
+				labelValues...,
+			)
+			ch <- prometheus.MustNewConstMetric(
+				c.CPUMax,
+				prometheus.GaugeValue,
+				max,
+				labelValues...,
+			)
+		}(instance)
 	}
-
+	wg.Wait()
 }
 
 // Describe implements Collector interface and is called by Prometheus to describe metrics
 func (c *InstanceCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- c.Count
-	ch <- c.CPU
+	ch <- c.CPUAve
+	ch <- c.CPUMax
 }
