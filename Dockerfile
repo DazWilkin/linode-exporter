6f6f6c9f36947c40cec3dfe462722eabe8dcb11f
DazWilkin <daz.wilkin@gmail.com>
2019-12-17 17:02:27 -0800

    Add build-args to provide main.go OS and Commit to main.go

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,22 @@
 ARG GOLANG_VERSION=1.13
+
 FROM golang:${GOLANG_VERSION} as build
+
+ARG VERSION=""
+ARG COMMIT=""
 
 WORKDIR /linode-exporter
 
-COPY go.mod .
+# Local go.mod includes: replace github.com/linode/linodego => .../linodego
+RUN echo "module github.com/DazWilkin/linode-exporter\ngo 1.13\nrequire (\n)\n" > ./go.mod
+
 COPY main.go .
 COPY collector ./collector
 
 RUN CGO_ENABLED=0 GOOS=linux \
-    go build -a -installsuffix cgo \
+    go build \
+    -ldflags "-X main.OSVersion=${VERSION} -X main.GitCommit=${COMMIT}" \
+    -a -installsuffix cgo \
     -o /go/bin/linode-exporter \
     ./main.go
 
--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-docker build --rm --file=Dockerfile --tag=dazwilkin/linode-exporter:$(git rev-parse HEAD) . && \
+export VERSION=$(uname --kernel-release) COMMIT=$(git rev-parse HEAD) && \
+docker build --rm --file=Dockerfile --build-arg=VERSION="${VERSION}" --build-arg=COMMIT="${COMMIT}" --tag=dazwilkin/linode-exporter:${COMMIT} . && \
 docker push dazwilkin/linode-exporter:$(git rev-parse HEAD) && \
 sed --in-place "s|dazwilkin/linode-exporter:[0-9a-f]\{40\}|dazwilkin/linode-exporter:$(git rev-parse HEAD)|g" ./docker-compose.yaml &&
 sed --in-place "s|dazwilkin/linode-exporter:[0-9a-f]\{40\}|dazwilkin/linode-exporter:$(git rev-parse HEAD)|g" ./README.md
