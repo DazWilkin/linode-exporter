61539fbfb1de260f843fa2479d7433dc2c6ddb3d
DazWilkin <daz.wilkin@gmail.com>
2023-02-15 12:46:41 -0800

    Updated

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 export VERSION=$(uname --kernel-release) COMMIT=$(git rev-parse HEAD) && \
 docker build --rm --file=Dockerfile --build-arg=VERSION="${VERSION}" --build-arg=COMMIT="${COMMIT}" --tag=dazwilkin/linode-exporter:${COMMIT} . && \
-docker push dazwilkin/linode-exporter:$(git rev-parse HEAD) && \
+docker push ghcr.io/dazwilkin/linode-exporter:$(git rev-parse HEAD) && \
 sed --in-place "s|dazwilkin/linode-exporter:[0-9a-f]\{40\}|dazwilkin/linode-exporter:$(git rev-parse HEAD)|g" ./docker-compose.yaml &&
 sed --in-place "s|dazwilkin/linode-exporter:[0-9a-f]\{40\}|dazwilkin/linode-exporter:$(git rev-parse HEAD)|g" ./README.md
